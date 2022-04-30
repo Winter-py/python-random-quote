f3bdde49b40c3cfa8ac99a6428c4f7420c3b2492
Steven Owatemi <isosteven@gmail.com>
2022-04-30 14:19:13 +0100

    take off the training wheels

--- get-quote.py
+++ get-quote.py
@@ -6,10 +6,12 @@ def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last = 13
+  last = 17
   rnd = random.randint(0, last)
+  arnd = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd] + quotes[arnd])
+  
 
 if __name__== "__main__":
   primary()
